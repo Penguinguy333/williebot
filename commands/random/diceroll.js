5dfec2c920b5ce9cbb860ffeabdb20c1ba57eb6f
Penguinguy333 <35020311+Penguinguy333@users.noreply.github.com>
2018-01-02 20:59:55 -0800

    Add files via upload

--- commands/random/diceroll.js
+++ commands/random/diceroll.js
@@ -1,19 +1,50 @@
-const commando = require('discord.js-commando');
+const commando = require('discord.js-commando')
+const _ = require('lodash')
 
-class DiceRollCommand extends commando.Command {
-    constructor(client) {
-        super(client, {
+ class DiceRollCommand extends commando.Command {
+
+    constructor(bot) {
+        super(bot, {
             name: 'roll',
             group: 'random',
             memberName: 'roll',
-            description: 'Rolls a die'
-        });
+            description: 'Rolls a dice.'
+        })
     }
 
     async run(message, args) {
-        var roll = Math.floor(Math.random() * 6) + 1;
-        message.reply("You rolled a " + roll);
+        let roll = args.split(' ')
+        let hasNumber = /^[0-9]$/
+
+        if (roll[0] || roll[1]) {
+            if (!/^[0-9]+$/.test(roll[0]) && !/^[0-9]+$/.test(roll[1])) {
+                console.log('roll[1] -> '+ !/^[0-9]+$/.test(roll[0])) // returns true
+                console.log('roll[2] -> '+ !/^[0-9]+$/.test(roll[1])) // returns true
+
+                message.reply('[DEBUG] Syntax Error input must be a number')
+                return
+            }
+        }
+        if (roll.length >= 3) {
+            message.reply('[DEBUG] Syntax Error cannot use more than 2 parameters')
+            return
+        }
+        if (roll[0] > 1000000 || roll[1] > 1000000) {
+            message.reply('Unfortunately for you, computers have a limited amount of memory, so unless you want me to run out, stop sending ludicrous numbers. Thanks.')
+            return
+        }
+       if (message.content.match(/^!roll$/)) {
+           message.reply('rolled ' + _.random(1, 6))
+       }
+       if (message.content.match(/^!roll [0-9]+\b/)) {
+           message.reply('rolled ' + _.random(1, roll[0]))
+       }
+       if (message.content.match(/^!roll ([0-9]*) ([0-9]*)+\b/)) {
+           message.reply('rolled ' + _.random(roll[0], roll[1]))
+       }
+
     }
+
 }
 
-module.exports = DiceRollCommand;
+module.exports = DiceRollCommand
