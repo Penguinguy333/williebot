b75bc78856ccaf98a8a1f0e5ac208ddc3ec0aa3d
Penguinguy333 <35020311+Penguinguy333@users.noreply.github.com>
2020-12-10 10:51:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const commando = require('discord.js-commando');
 const bot = new commando.Client();
 
 bot.registry.registerGroup('random', 'Random');
-bot.registry.registerGroup('big willies', 'Big Willies');
+bot.registry.registerGroup('g', 'g');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
