d74d426e0b9b4926bda530e787f588447f20e41d
Penguinguy333 <35020311+Penguinguy333@users.noreply.github.com>
2020-12-10 10:46:55 -0800

    Update williesize.js

--- commands/bigwillies/williesize.js
+++ commands/bigwillies/williesize.js
@@ -3,7 +3,7 @@ const commando = require('discord.js-commando');
 class WillieSizeCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'yesorno',
+            name: 'g',
             group: 'big willies',
             memberName: 'williesize',
             description: 'Shows willie size.'
@@ -13,13 +13,13 @@ class WillieSizeCommand extends commando.Command {
 async run(message, args) {
         var roll = Math.floor(Math.random() * 3) + 1;
         if (roll == 1) {
-            message.channel.send("no");
+            message.channel.send(message.author + " is smely lol");
         }
         if (roll == 2) {
-            message.channel.send("yes")
+            message.channel.send("https://media.discordapp.net/attachments/369924551502594048/777884744519516220/image0.gif")
         }
         if (roll == 3) {
-            message.channel.send("you are smely")
+            message.channel.send("https://media.discordapp.net/attachments/369924551502594048/777884744519516220/image0.gif")
         }
 
     }
